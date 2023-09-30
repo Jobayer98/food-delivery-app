92c7467f6110dcc33d5917a6492e984d497372c3
Jobayer98 <joobayerrahman0220@gmail.com>
2023-09-30 13:56:38 +0600

    add user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,96 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+interface User {
+    name: string,
+    email: string,
+    password: string,
+    address?: string,
+    phone?: string,
+    role: string,
+    tokens: string[],
+    generateToken(): string
+}
+
+const userSchema = new mongoose.Schema<User>({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value: string) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Email is invalid");
+            }
+        }
+        
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [6, "Password must be at least 6 characters"],
+        trim: true,
+        validate(password: string) {
+            const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+            if (!passwordRegex.test(password)) {
+                throw new Error("Password must contain at least one upper and lower letter, one number and one special character");
+            }
+
+            return passwordRegex.test(password);
+        }
+    },
+    address: {
+        type: String,
+        trim: true,
+        default: null
+    },
+    phone: {
+        type: String,
+        trim: true,
+        default: null
+    },
+    role: {
+        type: String,
+        default: "user"
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+
+})
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+userSchema.methods.comparePassword = async function (password: string) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateToken = function () {
+    if(!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not defined");
+    }
+
+    return jwt.sign({ _id: this._id, email: this.email }, process.env.JWT_SECRET);
+}
+
+const User = mongoose.model<User>("User", userSchema);
+
+export default User;
