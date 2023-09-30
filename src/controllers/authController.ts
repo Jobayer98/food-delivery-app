f93cd56f3a512c7dddeaf2cc9dd189672e18a10b
Jobayer98 <joobayerrahman0220@gmail.com>
2023-09-30 14:04:39 +0600

    add auth controller

---
+++ src/controllers/authController.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import User from "../models/userModel";
+import CustomError from "../utility/CustomError";
+
+export const authController = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const user = await User.create(req.body);
+        const token = user.generateToken();
+
+        res.cookie("access_token", 'Bearer ' + token, {
+            httpOnly: true,
+            maxAge: 30000, // 30 seconds
+
+        })
+        res.status(201).json({
+            success: true,
+            data: user,
+        })
+    } catch (error: any) {
+        next( new CustomError(error, 400) );
+    }
+}
