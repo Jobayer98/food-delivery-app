166dddbd9535b858e2fe86537226bc0e85f31e2c
Jobayer98 <joobayerrahman0220@gmail.com>
2023-09-30 15:23:18 +0600

    add login controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,10 +2,10 @@ import { Request, Response, NextFunction } from "express";
 import User from "../models/userModel";
 import CustomError from "../utility/CustomError";
 
-export const authController = async(req: Request, res: Response, next: NextFunction) => {
+export const signup = async(req: Request, res: Response, next: NextFunction) => {
     try {
         const user = await User.create(req.body);
-        const token = user.generateToken();
+        const token =  user.generateToken();
 
         res.cookie("access_token", 'Bearer ' + token, {
             httpOnly: true,
@@ -15,8 +15,40 @@ export const authController = async(req: Request, res: Response, next: NextFunct
         res.status(201).json({
             success: true,
             data: user,
+            token
         })
     } catch (error: any) {
         next( new CustomError(error, 400) );
+    }
+}
+
+export const login = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new CustomError("Account doesn't exist", 400);
+        }
+
+        const isMatch = user.comparePassword(password);
+
+        if(!isMatch){
+            throw new CustomError("invalid credentials", 401);
+        }
+
+        const token = user.generateToken();
+
+        res.cookie("access_token", 'Bearer ' + token, {
+            httpOnly: true,
+            maxAge: 30000, // 30 seconds
+        })
+        res.status(200).json({
+            success: true,
+            data: user,
+            token
+        })
+    } catch (error: any) {
+        next( new CustomError(error, 400) );
+        
     }
 }
