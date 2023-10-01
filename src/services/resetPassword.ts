c7ed49c4df974667fcc117fac3c967f8ab971e1f
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-01 08:38:48 +0600

    config nodemailer

---
+++ src/services/resetPassword.ts
@@ -0,0 +1,35 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false,
+  auth: {
+    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+    user: "abdullah91@ethereal.email",
+    pass: "UCRzsnpejhz6WyNDQZ",
+  },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+export const main = async(email:string) : Promise<any> => {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: 'rashedkhanjubayer018@gmail.com', // sender address
+    to: email, // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+return info;
+//   console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  //
+  // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
+  //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
+  //       <https://github.com/forwardemail/preview-email>
+  //
+}
+
+// main().catch(console.error);
