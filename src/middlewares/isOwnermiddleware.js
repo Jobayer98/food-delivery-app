42f6b9b1b63e8fc9acb91e48993970aa99eef5a7
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-01 16:57:01 +0600

    add isOwner middleware

---
+++ src/middlewares/isOwnermiddleware.js
@@ -0,0 +1,17 @@
+const User = require("../models/userModel");
+const CustomError = require("../utility/CustomError");
+
+const isOwner = async(req, res, next) => {
+    try {
+        const owner = await User.findById(req.user._id );
+        if (!(owner.role === "owner")){
+            throw new CustomError("You are not an owner", 400);
+        }
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = isOwner;
