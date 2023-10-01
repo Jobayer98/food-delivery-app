d591df8f545289a05e2bad2096a2e178b1853bab
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-01 08:37:59 +0600

    add resetPassword controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,21 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/userModel";
 import CustomError from "../utility/CustomError";
+import { main } from "../services/resetPassword";
 
 export const signup = async(req: Request, res: Response, next: NextFunction) => {
     try {
-        const user = await User.create(req.body);
-        const token =  user.generateToken();
+        const {email} = req.body;
+        let user = await User.findOne({ email });
+        if (user) {
+            throw new CustomError("Account already exists", 400);
+        }
 
-        res.cookie("access_token", 'Bearer ' + token, {
-            httpOnly: true,
-            maxAge: 30000, // 30 seconds
+        user = await User.create(req.body);
+        // const token =  user.generateToken();
 
-        })
+        // res.cookie("access_token", 'Bearer ' + token, {
+        //     httpOnly: true,
+        //     maxAge: 30000, // 30 seconds
+
+        // })
         res.status(201).json({
             success: true,
             data: user,
-            token
+            
         })
     } catch (error: any) {
         next( new CustomError(error, 400) );
@@ -36,17 +43,44 @@ export const login = async(req: Request, res: Response, next: NextFunction) => {
             throw new CustomError("invalid credentials", 401);
         }
 
-        const token = user.generateToken();
+        // const token = user.generateToken();
 
-        res.cookie("access_token", 'Bearer ' + token, {
-            httpOnly: true,
-            maxAge: 30000, // 30 seconds
-        })
+        // res.cookie("access_token", 'Bearer ' + token, {
+        //     httpOnly: true,
+        //     maxAge: 30000, // 30 seconds
+        // })
         res.status(200).json({
             success: true,
             data: user,
-            token
+            
         })
+    } catch (error: any) {
+        next( new CustomError(error, 400) );
+        
+    }
+}
+
+export const logout = async(req: Request, res: Response, next: NextFunction) => {
+    
+}
+
+export const resetPassword = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new CustomError("Account doesn't exist", 400);
+        }
+
+       const mail = await main(email)
+
+       res.status(200).json({
+           success: true,
+           data: mail.messageId,
+       })
+
+
     } catch (error: any) {
         next( new CustomError(error, 400) );
         
