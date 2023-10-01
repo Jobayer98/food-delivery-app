5017fe7362eb17a2aa4a07bcb71c7afe9d0bf279
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-01 16:54:38 +0600

    connect app with router

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require('cors');
 
 const authRouter = require("./routes/authRoute");
-// import CustomError from './utility/CustomError';
+const restaurantRouter = require("./routes/restaurantRoute");
+const orderRouter = require("./routes/orderRoute");
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(cors())
 app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1", restaurantRouter);
+app.use("/api/v1", orderRouter);
 
 // error handler
 app.use((err, req, res, next)=>{
