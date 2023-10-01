abd89d7032fee51c832571bb93ead19a74994191
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-01 11:22:37 +0600

    add order controller

---
+++ src/controllers/orderController.ts
@@ -0,0 +1,30 @@
+import {NextFunction, Request, Response} from "express"
+import CustomError from "../utility/CustomError"
+import OrderModel from "../models/orderModel"
+
+export const createOrder = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const order = await OrderModel.create({...req.body, customerId: req.user._id});
+        res.status(201).json({
+            success: true,
+            data: order
+        })
+    } catch (error: any) {
+        next( new CustomError(error, 400) );
+    }
+}
+
+export const showOrders = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const orders = await OrderModel.find({customerId: req.user._id});
+        if (!orders){
+            throw new CustomError("Orders not found", 404);
+        }
+        res.status(200).json({
+            success: true,
+            data: orders
+        })
+    } catch (error: any) {
+        next( new CustomError(error, 400) );
+    }
+}
