0bce23f34b584c5c87649dd2c9908c5f58815b55
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-01 10:03:54 +0600

    add devlivery model

---
+++ src/models/deliveryModel.ts
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+const deliverySchema = new Schema({
+    location: {
+       lat: {
+           type: Number,
+           required: true
+       },
+       long: {
+           type: Number,
+           required: true
+       }
+    },
+    arrivalTime: {
+        type: Date,
+    },
+    orderId: {
+        type: Schema.Types.ObjectId,
+        ref: "Order",
+        required: true,
+    },
+    driverId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    }
+})
+
+const DeliveryModel = model("Delivery", deliverySchema);
+
+export default DeliveryModel;
