383c5be0a4e380d26d8f05541a2f14caaa934cf1
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-01 08:37:16 +0600

    add password-reset route

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
-import {signup, login} from "../controllers/authController";
+import {signup, login, resetPassword} from "../controllers/authController";
 
 const router = express.Router();
 
 router.post("/signup", signup)
 router.post("/login", login)
+router.post("/reset", resetPassword)
 
 export default router;
