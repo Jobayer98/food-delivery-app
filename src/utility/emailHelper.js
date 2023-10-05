321ff0223f68a7bd614b2aa8909324ff32632f91
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-06 00:06:07 +0600

    add mailtrap smpt service

---
+++ src/utility/emailHelper.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer");
+
+const mailHelper = async(options) => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMPT_HOST,
+        port:process.env.SMPT_PORT,
+        auth: {
+          user: process.env.SMPT_USER,
+          pass: process.env.SMPT_PASS,
+        },
+      });
+      
+      const message = {
+        from: 'rashedkhanjubayer018@gmail.com', // sender address
+        to: options.email, // list of receivers
+        subject: options.subject, // Subject line
+        text: options.message, // plain text body
+      }
+
+      await transporter.sendMail(message)
+      
+}
+
+module.exports = mailHelper
