7ef82a421bef6a857279e6f6401e9253d59fb8be
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-06 00:04:40 +0600

    add user route

---
+++ src/routes/userRoute.js
@@ -0,0 +1,13 @@
+const express = require( "express")
+const auth = require("../middlewares/auth.middleware")
+const {userDashboard, updateUserInfo, forgotPassword, resetPassword, updateUserPassword} = require("../controllers/userController")
+
+const router = express.Router();
+
+router.get("/userdashboard", auth, userDashboard)
+router.patch("/user/update", auth, updateUserInfo)
+router.patch("/user/update-password", auth, updateUserPassword)
+router.get("/forgotpassword", auth, forgotPassword)
+router.post("/password/reset/:token", auth, resetPassword)
+
+module.exports=router;
