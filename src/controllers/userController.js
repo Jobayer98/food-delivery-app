f8a712977753187966aa490bfff5b259493afa12
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-05 23:59:40 +0600

    add user controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,136 @@
+const User = require("../models/userModel");
+const CustomError = require("../utility/CustomError");
+const mailHelper = require("../utility/emailHelper");
+const crypto = require("crypto");
+
+const userDashboard = async(req, res, next) => {
+    try {
+        const user = await User.find(req.user._id);
+
+        res.status(200).json({
+            success: true,
+            data: user
+        })
+    } catch (error) {
+        next( new CustomError(error, 400) );
+    }
+}
+
+const updateUserInfo = async(req, res, next) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.user._id, req.body, {new: true, runValidators: true});
+
+        res.status(200).json({
+            success: true,
+            data: user
+        })
+    } catch (error) {
+        next( new CustomError(error, 400) );
+    }
+} 
+
+const updateUserPassword = async(req, res, next) => {
+    const {oldPassword, newPassword} = req.body;
+    try {
+        const user = await User.findById(req.user._id);
+
+        if(!user){
+            return next( new CustomError("User not found", 404));
+        }
+
+        const isMatch = await user.comparePassword(oldPassword);
+
+        if(!isMatch){
+            return (new CustomError("Password update failed", 401));
+        }
+
+        user.password = newPassword;
+        await user.save(); 
+        
+        res.status(200).json({
+            success: true,
+            data: user
+        })
+    } catch (error) {
+        return next( new CustomError(error, 400) );
+    }
+}
+
+const forgotPassword = async(req, res, next) => {
+    
+    const user = await User.findOne({ email: req.body.email });
+    if(!user){
+        return next(new CustomError("User not found", 404));
+    }
+
+    const forgotToken = await user.getforgotPasswordToken();
+    await user.save({validateBeforeSave: false});
+
+    const resetUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${forgotToken}`
+
+    try {
+        mailHelper({
+            email: user.email,
+            subject: "Reset Password",
+            message: resetUrl
+        })
+    
+        await user.save();
+        res.status(200).json({
+            success: true,
+            msg: "Password reset link sent to your email"
+        })
+        
+    } catch (error) {
+        user.forgotPasswordToken = undefined;
+        user.forgotPasswordTokenExpiry = undefined;
+        await user.save({validateBeforeSave: false});
+
+        return next( new CustomError(error, 400) );
+    }
+}
+
+const resetPassword = async(req, res, next) => {
+    try {
+        const { token } = req.params;
+        const { password, confirmPassword } = req.body;
+
+        if(!password || !confirmPassword){
+            return next(new CustomError("Please enter password and confirm password", 400));
+        }
+
+        const encryptedToken = crypto.createHash("sha256").update(token).digest("hex");
+
+        const user = await User.findOne({ forgotPasswordToken: encryptedToken, forgotPasswordTokenExpiry: { $gt: Date.now() } });
+
+        if(!user){
+            return next(new CustomError("Password reset token is invalid or has expired", 400));
+        }
+
+        if(password !== confirmPassword){
+            return next(new CustomError("Passwords do not match", 400));
+        }
+        console.log(user.password)
+        user.password = password;
+        user.forgotPasswordToken = undefined;
+        user.forgotPasswordTokenExpiry = undefined;
+        await user.save();
+        console.log(user.password)
+
+        res.status(200).json({
+            success: true,
+            msg: "Password reset successfully"
+        })
+
+    } catch (error) {
+        return next( new CustomError(error, 400) );
+    }
+}
+
+module.exports = {
+    userDashboard,
+    updateUserInfo,
+    updateUserPassword,
+    forgotPassword,
+    resetPassword
+}
